51fda2f8f2dc94a40f633f17e45d27a0c9e2341a
Antônio Pedro <90531402+LkDarkness@users.noreply.github.com>
2021-12-12 00:45:55 -0300

    Create uptime.js

---
+++ SlashCommands/info/uptime.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "uptime",
+    description: "Ver quanto tempo eu estou online",
+    type: "CHAT_INPUT",
+
+run: async (client, interaction, args) => {
+  let totalSeconds = client.uptime / 1000;
+  let days = Math.floor(totalSeconds / 86400);
+  let hours = Math.floor(totalSeconds / 3600);
+  totalSeconds %= 3600;
+  let minutes = Math.floor(totalSeconds / 60);
+  let seconds = totalSeconds % 60;
+
+  let uptime = `> <a:estrela:905916572684656660> **|** **${days.toFixed()}** dias **${hours.toFixed()}** horas **${minutes.toFixed()}** minutos **${seconds.toFixed()}** segundos`;
+
+  interaction.followUp(`> **__~~Tempo de Atividade~~__** <a:uptime:905915820041981973>
+  > <a:mih_seta_esquerda:903320549864923146> Estou online há: \n${uptime}`);
+}
+}
